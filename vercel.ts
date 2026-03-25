1ce6faf409778e630526353b3893d3ab58094545
Andrey20Allyson <andreyuser47@gmail.com>
2026-03-25 18:40:06 -0300

    e

---
+++ vercel.ts
@@ -0,0 +1,29 @@
+import type { VercelConfig } from "@vercel/config";
+
+const config: VercelConfig = {
+    functions: {
+        "api/**/*.js": {
+            "maxDuration": 60
+        }
+    }
+}
+
+const env = process.env.NODE_ENV;
+const isVercel = env === "production";
+
+console.log(env);
+
+if (isVercel) {
+    config.rewrites = [
+        {
+            "source": "/api/(.*)",
+            "destination": "/api/$1"
+        },
+        {
+            "source": "/(.*)",
+            "destination": "/"
+        }
+    ]
+}
+
+export default config;
