008f5e796837386f3aaef92abdb1d267a2d41c9c
Andrey20Allyson <andreyuser47@gmail.com>
2026-03-25 17:02:13 -0300

    x

--- api/infra/comanda-api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-    baseURL: process.env.MYTENDER_HOST,
-    headers: {
-        "Content-Type": "application/json",
-        "apikey": process.env.MYTENDER_APIKEY,
-    }
-});
